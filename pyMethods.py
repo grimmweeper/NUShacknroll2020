f717cc1932f3771c9e2c237681a636995987fbe4
Le Xuan <soong.lexuan6@gmail.com>
2020-01-18 23:57:37 +0800

    lexuan

--- pyMethods.py
+++ pyMethods.py
@@ -2,7 +2,7 @@ import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import firestore
 from os import path
-# from flask import Flask, render_template, request
+from flask import Flask, render_template, request
 
 basepath = path.dirname(__file__)
 filepath = path.abspath(path.join(basepath, ".", ".", "schedulo-18432-621fa7d3e711.json"))
@@ -12,16 +12,13 @@ cred = credentials.Certificate(filepath)
 
 class DB:
     def __init__(self):
+        print("hihihi")
         firebase_admin.initialize_app(cred)
         self.db = firestore.client()
 
-    def check_if_board_populated(self,board_name):
-        docs = self.db.collection(board_name).stream()
-        populated = False
-        for doc in docs:
-            populated = True
-            break
-        return populated
+    def board_ref(self,board_name):
+        return self.Board(self,board_name)
+
 
     class Board:
         def __init__(self,outer_instance,board_name):
@@ -29,9 +26,9 @@ class DB:
             self.db = outer_instance.db
             self.board = self.db.collection(board_name)
 
-        def create_from_template(self):
-            coll = self.board
-            sections = coll.document("sections")
+        def create_from_template(self,board_name):
+            
+            sections = self.board.document("sections")
 
             todoData = {
                 "title": "todo",
@@ -76,8 +73,8 @@ class DB:
                 "done": doneData
             })
 
-            coll.document("members").set({})
-            coll.document("agenda").set({})
+            self.board.document("members").set({})
+            self.board.document("agenda").set({})
 
         def read_document(self,document_name):
             return self.board.document(document_name).get().to_dict()
@@ -92,12 +89,12 @@ class DB:
             self.board.document(document_name).set(data)
 
         def add_task(self, section_name, msg, emoji):
-            data = self.read_tasks
+            data = self.read_tasks()
             section = data[section_name]
             section[msg] = emoji
             self.write_document("sections", data)
 
-        def add_member(self, handle, name, color="white", emoji):
+        def add_member(self, handle, name, emoji, color="white"):
             data = self.read_members()
             data[handle] = {
                 'name': name,
@@ -106,16 +103,31 @@ class DB:
             }
             self.write_document("members", data)
 
-# db = DB()
-# print(db.check_if_board_populated("test4"))
-#
-# board = db.Board(db, "test4")
-# data = board.read_document("sections")
-# print(data['done']['body'])
+        # def delete_task(self, section_name, msg):
 
-# app = Flask(__name__)
-#
-# @app.route('/<boardID>/',methods=["GET,POST"])
-# def echo():
-#     if request.method == "POST":
-#         print(request.data)
+
+# db = DB()
+# board = db.Board(db,"test4")
+# print(board.get_members)
+
+app = Flask(__name__)
+
+@app.route('/',methods=["GET","POST"])
+def echo():
+    if request.method == "POST":
+        print("next is requesting data")
+        data = request.get_data().decode("utf-8")
+        print(data)
+        board = DB().board_ref(data)
+        print(board.read_document("sections"))
+        return board.read_document("sections")
+    else:
+        return {"title": "To-Do","body":["lexy","gab","weepz"],"color":"red","height":"300px","width":"100px","left":"200px","top":"100px"}
+
+# @app.route('/addTask/',methods=["GET","POST"])
+# def add_task_flask()
+
+@app.route('/index/',methods=["GET","POST"])
+def ind():
+    return render_template("main.html")
+
